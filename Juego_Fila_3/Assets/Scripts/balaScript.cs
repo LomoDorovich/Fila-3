4c2841670b82877dc1f1efeced4c64c9cbf33f2c
LomoDorovich <DorovichGranada@gmail.com>
2014-11-19 16:52:05 +0100

    Modificación sprite personaje.

--- Juego_Fila_3/Assets/Scripts/balaScript.cs
+++ Juego_Fila_3/Assets/Scripts/balaScript.cs
@@ -1,29 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class balaScript : MonoBehaviour {
+public class bala : MonoBehaviour {
+	
 	public Vector2 velocity = new Vector2(5,0);
 	public GameObject particulas;
-
+	//public GameObject blood;
+	//public int damage = 5;
 	// Use this for initialization
 	void Start () {
+		//rigidbody2D.AddForce = velocity * transform.localScale.x;
 		rigidbody2D.AddForce (velocity * transform.localScale.x, ForceMode2D.Impulse);
-
-
+		//Invoke ("onDestroy", 0.2f);
 	}
-	void OnCollisionEnter2D(Collision2D target){
-				Destroy (gameObject);
-		}
-	void onDestroy(){
-
-				var clone = Instantiate (particulas, transform.position, Quaternion.identity)as GameObject;
-				Destroy (clone, 1);
-
-				Destroy (gameObject);
-		}	 	
-
-	// Update is called once per frame
-	void Update () {
 	
+	void OnCollisionEnter2D(Collision2D target){
+		Destroy (gameObject);
+		//onDestroy();
+	}
+	
+	
+	
+	void onDestroy(){
+		var clone = Instantiate(particulas,transform.position,Quaternion.identity)
+			as GameObject;
+		Destroy(clone,1);
+		Destroy(gameObject);
 	}
 }
